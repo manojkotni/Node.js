f15ef45fad757d09caec0e14b30f411cb41a4ce7
manojkotni <99539907+manojkotni@users.noreply.github.com>
2022-06-27 13:01:59 +0530

    Rest Full api

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-var http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+const BrandName = require('./model')
+const app = express();
+
+app.use(express.json())
+mongoose.connect('mongodb://127.0.0.1:27017/',{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}).then( ()=> console.log('db connected')
+).catch(err =>console.log(err))
+
+app.post('/addbrands',async (req,res) => {
+    const {brandname} = req.body;
+        try{
+            const newData = new BrandName({brandname});
+             await newData.save();
+            return res.json(await BrandName.find())
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
 
 
- http.createServer((req, res)=>{
-     res.write(req.url);
-     res.end();
-     console.log('server running..')
- }).listen(8080) 
+app.get('/', (req,res) =>{
+    res.send('<h1>hello world!!!</h1>')
+})
+
+ app.listen(3000, ()=> console.log('server running..'))
